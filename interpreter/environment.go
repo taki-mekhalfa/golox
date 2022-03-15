51d925f58e9eb5999537342fb83686c190df0291
taki-mekhalfa <takiedd.mekhalfa@gmail.com>
2022-03-15 21:06:19 +0100

    interpreter: add assign function to differentiate between assignment and definition

--- interpreter/environment.go
+++ interpreter/environment.go
@@ -9,6 +9,14 @@ func (e *environment) define(name string, value interface{}) {
 	e.values[name] = value
 }
 
+func (e *environment) assign(name string, value interface{}) {
+	if _, ok := e.values[name]; ok {
+		e.values[name] = value
+		return
+	}
+	e.parent.assign(name, value)
+}
+
 func (e *environment) get(name string) (interface{}, bool) {
 	v, ok := e.values[name]
 	if !ok && e.parent != nil {
