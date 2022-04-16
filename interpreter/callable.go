92a23415155bdf0255b266e43107cb1572c298ea
taki-mekhalfa <takiedd.mekhalfa@gmail.com>
2022-04-16 15:21:48 +0200

    interpreter: add a built-in clock function

--- interpreter/callable.go
+++ interpreter/callable.go
@@ -4,8 +4,21 @@ import (
 	"time"
 )
 
+var (
+	clockFn = &clock{}
+)
+
 type callable interface {
 	call(*Interpreter, []interface{}) interface{}
 	// returns the function's arity
 	arity() int
+}
+
+type clock struct {
+}
+
+func (c *clock) arity() int { return 0 }
+
+func (c *clock) call(interpreter *Interpreter, args []interface{}) interface{} {
+	return time.Now().Unix()
 }
