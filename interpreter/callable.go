5caa5ba72c252a3e9eef8c2382bbd18cd708d9d7
taki-mekhalfa <takiedd.mekhalfa@gmail.com>
2022-04-16 17:53:05 +0200

    interpreter: add function as a callable type

--- interpreter/callable.go
+++ interpreter/callable.go
@@ -2,6 +2,8 @@ package interpreter
 
 import (
 	"time"
+
+	"github.com/taki-mekhalfa/golox/ast"
 )
 
 var (
@@ -21,4 +23,31 @@ func (c *clock) arity() int { return 0 }
 
 func (c *clock) call(interpreter *Interpreter, args []interface{}) interface{} {
 	return time.Now().Unix()
+}
+
+type function struct {
+	declaration *ast.Function
+}
+
+func (f *function) arity() int { return len(f.declaration.Params) }
+func (f *function) call(interpreter *Interpreter, args []interface{}) interface{} {
+	// save the current interpreter environment
+	previous := interpreter.env
+
+	// create a new environement exclusive to this function call starting-up from the global env
+	functionEnv := newEnvironment(interpreter.globals)
+	interpreter.env = functionEnv
+
+	// bind function parameters to arguments
+	for i, param := range f.declaration.Params {
+		functionEnv.define(param.Lexeme, args[i])
+	}
+
+	// exectue the function body
+	interpreter.Interpret(f.declaration.Body)
+
+	// recover back the interpreter environement
+	interpreter.env = previous
+
+	return nil
 }
